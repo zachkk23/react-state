bfb58aa38fe74f664db1667f90e0fa773fa835c6
zachkk23 <110347820+zachkk23@users.noreply.github.com>
2023-08-16 11:59:33 +0300

    react  state
    Description

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState} from 'react'
+import "./App.css"
+export default function App(){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const productData = [
+  {  id:1  , mo:true ,name:'porsche wheel' , image:'wheel1.jpg', cost:'$20'},
+  {  id:2 , mo:true ,name:'ferrari wheel' , image:'wheel2.jpg', cost:'$20'},
+  {  id:3 , mo:true ,name:'Lamborghini wheel' , image:'wheel3.jpg', cost:'$20'},
+]
+ 
+const[squire,setSquire] =useState(productData)
+const data = squire.map(item => ( 
+    <div key={item.id} >
+    <img src={item.image} alt={item.name}/>
+    {item.name} {item.cost}
+    <div className='buy' onClick={() => Wheel(item.id)}>
+        { item.mo? <p>BUY</p> : <p>SOLD</p>}
+        </div>
+
+    </div>  
+))
+
+function Wheel(id) {
+   setSquire(prevSquire => {
+    const newSquires = []
+    for(let i = 0;i < prevSquire.length ; i ++){
+        const currentSquires = prevSquire[i]
+        if(currentSquires.id === id){
+           const updatedSquire = {
+            ...currentSquires,
+            mo:!currentSquires.mo
+           }
+           newSquires.push(updatedSquire)
+        }else{
+            newSquires.push(currentSquires)
+        }
+    }
+    return newSquires
+   } )
 }
 
-export default App;
+    return(       
+        <div>
+            <h1>Product:tyre</h1>
+              {data}
+        </div>
+    )
+}
+   
